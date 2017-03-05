e692c903c5a9264eef55671898359db32d8b7969
techvisio <gusain.pradeep1986@gmail.com>
2017-03-05 22:37:36 +0530

    Added util stuff

---
+++ app/utils/constants.js
@@ -0,0 +1,6 @@
+module.exports=function(){
+    return {
+        question_level_high:"HIGH",
+
+    };
+}
---
+++ app/utils/logger.js
@@ -0,0 +1,20 @@
+var  winston = require('winston');
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.File)({
+      timestamp: tsFormat,
+      colorize: true,
+    })
+  ]
+});
+module.exports=function(){
+    return {
+           debug:debug
+    };
+
+    var debug=function(msg){
+      logger.debug(msg);
+    }
+}
