a2663c647e5f25ab3f4553b604b4c718be5dc7e2
sgusain <sgusain91gmail.com>
2017-03-08 22:59:20 +0530

    changes in logger.js

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -10,12 +10,47 @@ const logger = new (winston.Logger)({
   ]
 });
 
-module.exports=function(){
+module.exports=(function(){
     return {
-           debug:debug
+           debug:debug,
+           error:error,
+           silly:silly,
+           info:info,
+           verbose:verbose,
+           warn:warn,
+           setLoggerLevel:setLoggerLevel,
+           getLoggerLevel:getLoggerLevel
     };
 
     var debug=function(msg){
       logger.debug(msg);
     }
-}
+    var error=function(msg){
+      logger.error(msg);
+    }
+    var silly=function(msg){
+      logger.silly(msg);
+    }
+    var info=function(msg){
+      logger.info(msg);
+    }
+    var verbose=function(msg){
+      logger.verbose(msg);
+    }
+    var warn=function(msg){
+      logger.warn(msg);
+    }
+
+    function getLoggerLevel(){
+      return logger.level;
+    }
+    
+    function setLoggerLevel(levelValue){
+      if(levelValue=='debug' || levelValue=='info' || levelValue=='silly' || levelValue=='warn' || levelValue=='error'|| levelValue=='verbose'){
+         winston.level = levelValue;
+         return;
+      }
+
+       logger.error('Not vaild logger level');
+    }
+}())
